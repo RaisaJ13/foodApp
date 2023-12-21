b7c03af9cf5fcaeeab938b30b899d6255922c820
RaisaJ13 <152383228+RaisaJ13@users.noreply.github.com>
2023-12-21 12:16:02 +0600

    Update end_part.dart

--- end_part.dart
+++ end_part.dart
@@ -1,24 +1,49 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:food_app/logIn/log_in.dart';
 import 'package:food_app/my_button.dart';
+import 'package:food_app/signup_page.dart';
 
 
-class EndPart extends StatelessWidget{
+class EndPart extends StatefulWidget{
   const EndPart({Key?key}) : super(key : key);
 
+  @override
+  State<EndPart> createState() => _EndPartState();
+}
+
+class _EndPartState extends State<EndPart> {
   @override
   Widget build(BuildContext context){
     return Column(
       children: [
-        mybutton(
-          onPressed: (){},
-          text: " Log In",
+        Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: mybutton(
+                onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (c)=>const signup()));
+                },
+                text: " Sign up",
+
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: mybutton(
+                onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (c)=>const login()));
+                },
+                text: "Log In",
+              ),
+            ),
+          ],
         ),
-        SizedBox(height: 20,),
-        Text("Sig up"),
       ],
     );
   }
-
 }
